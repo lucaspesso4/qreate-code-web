4ac7c6cb9fcc5e36dac8b429bd92659d0b929067
Lucas Pessoa <lucaspesso4>
2022-01-10 11:00:04 -0300

    add: header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/logo.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header className="appbar__container">
+        <div>
+          <img alt="logo" src={logo} />
+        </div>
+        <h3 className="appbar__title">Simple, as you need</h3>
       </header>
-    </div>
+    </main>
   );
 }
 
